eb3f006a43fca34e95cca1b07791514a8c66932c
nolandex <stemapal1234@gmail.com>
2025-04-30 03:39:40 +0700

    Update Testimonials.tsx

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -1,10 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
-import { TwitterX } from "@/components/social-icons/icons";
-import { siteConfig } from "@/config/site";
 import { TestimonialsData } from "@/config/testimonials";
 import Image from "next/image";
-import Link from "next/link";
-import { RoughNotation } from "react-rough-notation";
 
 const Testimonials = ({ id, locale }: { id: string; locale: any }) => {
   return (
@@ -13,24 +9,8 @@ const Testimonials = ({ id, locale }: { id: string; locale: any }) => {
       className="flex flex-col justify-center items-center pt-16 gap-12 max-w-[88%]"
     >
       <div className="flex flex-col text-center max-w-xl gap-4">
-        <h2 className="text-center text-white">
-          <RoughNotation type="highlight" show={true} color="#2563EB">
-            {locale.title}
-          </RoughNotation>
-        </h2>
-        <p className="text-large text-default-500">
-          {/* Don't take our word for it. Here's what they have to say. */}
-          {locale.description1}{" "}
-          <Link
-            href={siteConfig.authors[0].twitter as string}
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-            className="text-primary underline"
-          >
-            {locale.description2}
-          </Link>
-          {locale.description3}
-        </p>
+        <h2 className="text-center text-white">{locale.title}</h2>
+        <p className="text-large text-default-500">{locale.description1}</p>
       </div>
       <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-4 overflow-hidden relative transition-all">
         {TestimonialsData.map((testimonial, index) => (
@@ -40,7 +20,7 @@ const Testimonials = ({ id, locale }: { id: string; locale: any }) => {
                 <div className="flex items-start gap-2">
                   <Image
                     src={testimonial.user.image}
-                    alt="maker"
+                    alt="user"
                     height={40}
                     width={40}
                     className="w-12 h-12 rounded-full object-cover object-top"
@@ -52,13 +32,6 @@ const Testimonials = ({ id, locale }: { id: string; locale: any }) => {
                     </p>
                   </div>
                 </div>
-                <Link
-                  href={`https://twitter.com/${testimonial.user.username}`}
-                  target="_blank"
-                  rel="noopener noreferrer nofollow"
-                >
-                  <TwitterX className="w-8 h-8" />
-                </Link>
               </div>
               <p className="dark:text-zinc-200 text-[14px]">
                 {testimonial.content}
