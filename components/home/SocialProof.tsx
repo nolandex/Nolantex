94d094e93ec35992bef8fff81f3d661d431cd1b3
nolandex <stemapal1234@gmail.com>
2025-04-30 04:03:24 +0700

    Update SocialProof.tsx

--- components/home/SocialProof.tsx
+++ components/home/SocialProof.tsx
@@ -3,23 +3,21 @@ import Image from "next/image";
 
 const SocialProof = ({ locale }: { locale: any }) => {
   return (
-    <section className="flex flex-col items-center justify-center gap-20 pt-16">
+    <section className="flex flex-col items-center justify-center gap-20 pt-16 w-full">
       <div className="flex flex-col items-center gap-5">
-        <div className="flex items-center justify-center">
-          {Makers.map((user, index) => {
-            return (
-              <Image
-                key={index}
-                src={user.image}
-                alt="User"
-                height={40}
-                width={40}
-                className="rounded-full -m-[5px] border border-white"
-              />
-            );
-          })}
+        <div className="flex items-center justify-center flex-wrap">
+          {Makers.map((user, index) => (
+            <Image
+              key={index}
+              src={user.image}
+              alt="User"
+              height={40}
+              width={40}
+              className="rounded-full -m-[5px] border border-white"
+            />
+          ))}
         </div>
-        <p className="text-sm text-slate-700 dark:text-slate-400">
+        <p className="text-sm text-slate-700 dark:text-slate-400 text-center">
           <span className="text-primary font-semibold text-base">1000+</span>{" "}
           {locale.maker}
         </p>
