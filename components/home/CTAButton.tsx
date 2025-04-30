1f48e3b60b76eb11649a91a7bfac5d7085fea48b
nolandex <stemapal1234@gmail.com>
2025-04-30 11:54:43 +0700

    Update CTAButton.tsx

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -3,12 +3,16 @@ import { RocketIcon } from "lucide-react";
 import Link from "next/link";
 
 const CTAButton = ({ locale }: { locale: any }) => {
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault(); // Mencegah perilaku default link
+    const pricingSection = document.getElementById("pricings");
+    if (pricingSection) {
+      pricingSection.scrollIntoView({ behavior: "smooth" }); // Scroll halus ke section
+    }
+  };
+
   return (
-    <Link
-      href="#Pricing"
-      target="_blank"
-      rel="noopener noreferrer nofollow"
-    >
+    <Link href="#Pricing" onClick={handleScroll}>
       <Button
         variant="default"
         className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
