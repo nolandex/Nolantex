5568f6ac1280f0e5564d15a0554415a80564548b
nolandex <stemapal1234@gmail.com>
2025-05-28 00:43:54 +0700

    Create page.tsx

---
+++ app/second/page.tsx
@@ -0,0 +1,7 @@
+export default function SecondPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to the Second Page!</h1>
+    </div>
+  );
+}
