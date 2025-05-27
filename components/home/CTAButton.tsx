5d20b9e0e1ddad4e5a1b2e5dc3eddcff4ae1e3d9
nolandex <stemapal1234@gmail.com>
2025-05-28 01:04:08 +0700

    Update CTAButton.tsx

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -4,12 +4,21 @@ import { Button } from "@/components/ui/button";
 import { RocketIcon } from "lucide-react";
 import Link from "next/link";
 
-const CTAButton = ({ locale, href = "/" }: { locale: any; href?: string }) => {
+const CTAButton = ({ locale }: { locale: any }) => {
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const pricingSection = document.getElementById("Pricing");
+    if (pricingSection) {
+      pricingSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <Link href={href}>
+    <Link href="#Pricing" onClick={handleScroll}>
       <Button
         variant="default"
         className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
+        aria-label="Get Boilerplate"
       >
         <RocketIcon />
         {locale.title}
