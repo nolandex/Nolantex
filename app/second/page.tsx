4d1310659a503d17a03becbdbacfeb026d15fb3e
nolandex <stemapal1234@gmail.com>
2025-05-28 06:19:53 +0700

    Delete app/second directory

--- app/second/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/second/page.tsx
-export default function SecondPage() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold">Halaman Kedua</h1>
-      <p>Anda berhasil navigasi ke halaman ini!</p>
-    </div>
-  );
-}
