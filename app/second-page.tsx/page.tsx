d34cb166abc9c81d213ab31c53aded9862207f33
nolandex <stemapal1234@gmail.com>
2025-05-28 06:20:39 +0700

    Create page.tsx

---
+++ app/second-page.tsx/page.tsx
@@ -0,0 +1,11 @@
+// app/second-page/page.tsx
+import React from 'react';
+
+export default function SecondPage() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Welcome to the Second Page!</h1>
+      <p>This is the content of your newly created page.</p>
+    </div>
+  );
+}
