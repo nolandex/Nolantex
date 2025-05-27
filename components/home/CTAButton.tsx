9267c16db04e52621a8f1f8b94f199b60067ea75
nolandex <stemapal1234@gmail.com>
2025-05-28 01:17:54 +0700

    Update CTAButton.tsx

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -3,27 +3,27 @@
 import { Button } from "@/components/ui/button";
 import { RocketIcon } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const CTAButton = ({ locale }: { locale: any }) => {
-  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement>) => {
+  const router = useRouter();
+
+  // Fungsi untuk handle navigasi
+  const handleNavigate = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    const pricingSection = document.getElementById("Pricing");
-    if (pricingSection) {
-      pricingSection.scrollIntoView({ behavior: "smooth" });
-    }
+    router.push("/second"); // Navigasi ke halaman kedua
   };
 
   return (
-    <Link href="#Pricing" onClick={handleScroll}>
-      <Button
-        variant="default"
-        className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
-        aria-label="Get Boilerplate"
-      >
-        <RocketIcon />
-        {locale.title}
-      </Button>
-    </Link>
+    <Button
+      onClick={handleNavigate}
+      variant="default"
+      className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
+      aria-label="Get Boilerplate"
+    >
+      <RocketIcon />
+      {locale.title}
+    </Button>
   );
 };
 
