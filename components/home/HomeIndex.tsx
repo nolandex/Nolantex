443635c50db1428f88be0992566e60a954ed7114
nolandex <stemapal1234@gmail.com>
2025-05-27 16:32:00 +0700

    Update HomeIndex.tsx

--- components/home/HomeIndex.tsx
+++ components/home/HomeIndex.tsx
@@ -2,7 +2,6 @@ import CTA from "@/components/home/CTA";
 import FAQ from "@/components/home/FAQ";
 import Feature from "@/components/home/Feature";
 import Hero from "@/components/home/Hero";
-import Pricing from "@/components/home/Pricing";
 import ScrollingLogos from "@/components/home/ScrollingLogos";
 import SocialProof from "@/components/home/SocialProof";
 import Testimonials from "@/components/home/Testimonials";
@@ -25,9 +24,6 @@ export default async function HomeIndex({ lang }: { lang: string }) {
 
       {/* USP (Unique Selling Proposition) */}
       <Feature id="Features" locale={dict.Feature} langName={langName} />
-
-      {/* Pricing */}
-      <Pricing id="Pricing" locale={dict.Pricing} langName={langName} />
 
       {/* Testimonials */}
       <Testimonials id="Testimonials" locale={dict.Testimonials} />
