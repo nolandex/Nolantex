3558cec82ac10ba32374653d699e2414bfac2726
nolandex <stemapal1234@gmail.com>
2025-05-27 23:55:42 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/layanan",
+        destination: "/id/layanan",
+        permanent: true,
+      },
+      {
+        source: "/en/layanan",
+        destination: "/id/layanan",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
