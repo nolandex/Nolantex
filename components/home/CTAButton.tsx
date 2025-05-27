078ccab0bf9e942342621263609a904c1ec649f7
nolandex <stemapal1234@gmail.com>
2025-05-28 01:23:01 +0700

    Update CTAButton.tsx

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -2,26 +2,20 @@
 
 import { Button } from "@/components/ui/button";
 import { RocketIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const CTAButton = ({ locale }: { locale: any }) => {
-  const router = useRouter();
-
-  const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    router.push("/second"); // Pastikan path ini sesuai
-  };
-
   return (
-    <Button
-      onClick={handleClick}
-      variant="default"
-      className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
-      aria-label="Get Boilerplate"
-    >
-      <RocketIcon />
-      {locale.title}
-    </Button>
+    <Link href="/second" passHref legacyBehavior>
+      <Button
+        variant="default"
+        className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
+        aria-label="Get Boilerplate"
+      >
+        <RocketIcon />
+        {locale.title}
+      </Button>
+    </Link>
   );
 };
 
