02f63eb2a3bb4989b2627df7cf6c776324af6fa9
nolandex <stemapal1234@gmail.com>
2025-05-29 18:19:44 +0700

    Update ThemeProvider.tsx

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -4,5 +4,9 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="light" {...props}>
+      {children}
+    </NextThemesProvider>
+  );
 }
