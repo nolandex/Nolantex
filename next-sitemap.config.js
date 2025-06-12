01b7da0dd180d2fb6a06e201b1f5e19ff8c228b7
nolandex <stemapal1234@gmail.com>
2025-06-12 12:16:58 +0700

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-  siteUrl:
-    process.env.NEXT_PUBLIC_SITE_URL || "https://landingpage.weijunext.com",
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-};
